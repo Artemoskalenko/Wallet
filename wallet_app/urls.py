07ecd2d00342eb960409d05133d345baf2a686fe
artem <artem.moskalenko.00@gmail.com>
2023-05-07 20:02:08 +0300

    add account form changes

--- wallet_app/urls.py
+++ wallet_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.MainPage.as_view(), name='main'),
+    path('create_account/', views.CreateAccount.as_view(), name='create_account'),
+
     # path('json', views.cur_update, name='cur_update'),
     # path('currencies', views.Currencies.as_view(), name='currencies'),
 
--- wallet_app/views.py
+++ wallet_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView
 
 from .models import Currency, Wallet
 from .tasks import currency_update_task
@@ -14,6 +14,27 @@ class MainPage(ListView):
     def get_queryset(self):
         return Wallet.objects.filter(user=self.request.user)
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context["currencies"] = Currency.objects.all()
+        return context
+
+
+class CreateAccount(CreateView):
+    def post(self, request, *args, **kwargs):
+        # field1 = request.POST.get('field1')
+        color = request.POST.get('color')
+        quantity = request.POST.get('quantity')
+        # print(color)
+        # print(quantity)
+        for item in request.POST:
+            print(item)
+
+
+
+        return redirect('main')
 
 # def cur_update(request):
 #     currency_update_task.delay()
